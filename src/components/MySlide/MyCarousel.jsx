4459a88dd47ca0c4760aa7cc558e71469035f6ab
DanielImgbi <imgbidanielgodspower@gmail.com>
2023-04-24 17:48:54 -0700

    my second commit

--- src/components/MySlide/MyCarousel.jsx
+++ src/components/MySlide/MyCarousel.jsx
@@ -5,59 +5,59 @@ import './MyCarousel.css'
 export const MyCarousel = () => {
   return (
     <div className=''>
-          <Carousel className='my-carousel d-flex justify-content-center'>
-              <Carousel.Item className='sub.slide'>
+          <Carousel>
+              <Carousel.Item >
                 <img
-                    className="d-block w-25"
+                    className="d-block w-100"
                     src="public/safsims-imagery/ACCE-Abuja.png"
                     alt="Slide image alt"
                 />
               </Carousel.Item>
-              <Carousel.Item className='sub-slide'>
+              <Carousel.Item >
                 <img
-                    className="d-block"
+                    className="d-block w-100"
                     src="public/safsims-imagery/Almustaqeem.png"
                     alt="Slide image alt"
                 />
               </Carousel.Item>
-              <Carousel.Item className='sub-slide'>
+              <Carousel.Item >
                 <img
-                    className="d-block"
+                    className="d-block w-100"
                     src="public/safsims-imagery/CHRYSOLITE.png"
                     alt="Slide image alt"
                 />
               </Carousel.Item>
-              <Carousel.Item className='sub-slide'>
+              <Carousel.Item >
                 <img
-                    className="d-block"
+                    className="d-block w-100"
                     src="public/safsims-imagery/Crescent-Pearls.png"
                     alt="Slide image alt"
                 />
               </Carousel.Item>
-              <Carousel.Item className='sub-slide'>
+              <Carousel.Item >
                 <img
-                    className="d-block"
+                    className="d-block w-100"
                     src="public/safsims-imagery/Edulyn-Schools.png"
                     alt="Slide image alt"
                 />
               </Carousel.Item>
-              <Carousel.Item className='sub-slide'>
+              <Carousel.Item >
                 <img
-                    className="d-block"
+                    className="d-block w-100"
                     src="public/safsims-imagery/Epitome.png"
                     alt="Slide image alt"
                 />
               </Carousel.Item>
-              <Carousel.Item className='sub-slide'>
+              <Carousel.Item >
                 <img
-                    className="d-block"
+                    className="d-block w-100"
                     src="public/safsims-imagery/I-scholars.png"
                     alt="Slide image alt"
                 />
               </Carousel.Item>
-              <Carousel.Item className='sub-slide'>
+              <Carousel.Item >
                 <img
-                    className="d-block"
+                    className="d-block w-100"
                     src="public/safsims-imagery/Prime-Scholars.png"
                     alt="Slide image alt"
                 />
